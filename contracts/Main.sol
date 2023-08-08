c2a4ea24a6c54bd741a6dd6f5231ea91a305116c
mcuadradome <legumi14asem@gmail.com>
2023-08-08 17:00:55 -0500

    add payment functions

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 import "./token/ERC20.sol";
 import "./utils/Context.sol";
 
-contract Main is ERC20 {
+contract Main {
 
    /* 0x5E3F773D4C0eAC9BbC410623DF5fAd45F08DCFA0 -> PRESTATARIO
       0x72f188f3767fE8FE835b7b22191f28A3b19562D1 -> INTERMEDIARIO
@@ -20,81 +20,114 @@ contract Main is ERC20 {
     // ERC20 private token;
     
     // Direcciones ;
-    address public contrato;
-    address public intermediary = 0x72f188f3767fE8FE835b7b22191f28A3b19562D1;
+    address payable  public contrato;
+    address payable private intermediary = payable(0x72f188f3767fE8FE835b7b22191f28A3b19562D1);
+    uint256 public maxEthPerTransaction = 1 ether; // Máximo 1 ETH por transacción
+
     //uint256 totalSupply_;
 
     event PaymentReceived(address from, uint256 amount);
     event PaymentSent(address to, uint256 amount);
  
 
-    constructor() payable ERC20("FlixCapital", "FLX") { 
-        //totalSupply_ = total;
+    constructor(){ 
+        // totalSupply_ = total;
         // token = new ERC20("MyToken", "MTK");
         owner = msg.sender;
-        contrato = address(this);
-       
+        contrato = payable(address(this));    
     }
 
-    function addEthToBalance(uint amount) payable external  {
-        require(msg.value > 0, "Debes enviar ETH");
-        require(msg.value == amount, "Valor ingresado no valido");
-        payable(msg.sender).transfer(contrato.balance);
-    }
-
-    modifier onlyOwner() {
+     modifier onlyOwner() {
         require(msg.sender == owner, "Solo el propietario puede ejecutar esta funcion.");
         _;
     }
 
-    function costValue(uint256 number) internal pure returns (uint256){
-        return number*(1 ether);
+    function isValidAddress(address _address) external view returns (bool) {
+        return (_address != address(0) && _address != address(contrato) && bytes20(_address).length == 20);
     }
 
-    function valueTransaction(uint256 number) internal pure returns (uint256){
-        // Calcula el 2% del número
-        return (number * 2) / 100;
+    // function addEthToBalance(uint256 amount) payable external onlyOwner()  {
+    //     require(msg.value > 0, "Debe enviar ETH");
+    //     require(msg.value ==  SafeMath.convertToWei(amount), "El valor ingresado no es valido");
+    //     require(this.contractBalance() > 0, "El contrato no tiene saldo para procesar la transaccion");
+
+    //     uint256 balance = SafeMath.add(this.contractBalance(), msg.value); 
+    //     payable(msg.sender).transfer(balance);
+    // }
+
+    // Función para agregar ETH a una dirección específica
+    function addEthToAddress(address payable _receiver) external payable onlyOwner() {
+        //require(_receiver != address(0), "La direccion del receptor no puede ser 0x0");
+        require(this.isValidAddress(_receiver), "La direccion del receptor no es valida");
+        require(msg.value > 0 && msg.value <= maxEthPerTransaction, "Cantidad de ETH no valida");
+        require(this.addressBalance(msg.sender) > 0, "La cuenta origen no tiene saldo"); 
+       
+        // Transferir ETH a la dirección específica
+        _receiver.transfer(msg.value);
+        emit PaymentSent(_receiver, msg.value);
+    }
+
+        // Función para dividir un monto entre dos direcciones
+    function payment(address payable _receiver) payable external onlyOwner(){
+        require(msg.value > 0 && msg.value <= maxEthPerTransaction, "Cantidad de ETH no valida");
+        require(this.addressBalance(msg.sender) == 0, "La cuenta origen no tiene saldo"); 
+        require(this.isValidAddress(_receiver), "La direccion del receptor no es valida");
+
+        uint256 twoPercent = SafeMath.discountTransaction(msg.value);
+        uint256 remainingAmount = SafeMath.sub(msg.value, twoPercent);
+
+        // Enviar el 2% a RECEIVER_1
+        intermediary.transfer(twoPercent);
+        emit PaymentReceived(_receiver, twoPercent);
+
+        // Enviar el restante a RECEIVER_2
+        _receiver.transfer(remainingAmount);
+        emit PaymentReceived(_receiver, remainingAmount);
     }
 
     // function balance() public view returns (uint256) {
     //     return token.balanceOf(contrato);
     // }
 
-    function GeneraTokens(address account, uint amount) public onlyOwner(){
-        _mint(account, amount);
-    }
+    // function GeneraTokens(address account, uint amount) public onlyOwner(){
+    //     _mint(account, amount);
+    // }
 
-    function paymentValue(address _to, uint256 _value) external payable {
+    // function paymentValue(address _to, uint256 _value) external payable {
  
-        uint coste = costValue(_value);
-        // Se requiere que el valor de ethers pagados sea equivalente al coste 
-        require (_value >= coste, "Compra menos Tokens o paga con mas Ethers.");
+    //     uint coste = costValue(_value);
+    //     // Se requiere que el valor de ethers pagados sea equivalente al coste 
+    //     require (_value >= coste, "Compra menos Tokens o paga con mas Ethers.");
         
-        // Diferencia a pagar 
-        uint256 returnValue = _value - coste;
+    //     // Diferencia a pagar 
+    //     uint256 returnValue = _value - coste;
         
-        // Tranferencia de la diferencia 
-        payable(_to).transfer(returnValue);
+    //     // Tranferencia de la diferencia 
+    //     payable(_to).transfer(returnValue);
 
-        // Tranferencia  el coste a la cuenta intermediaria 
-        payable(intermediary).transfer(coste);
+    //     // Tranferencia  el coste a la cuenta intermediaria 
+    //     payable(intermediary).transfer(coste);
        
-        // Obtener el balance de Tokens del contrato 
-        uint256 tbalance =  this.balanceOf(contrato);
+    //     // Obtener el balance de Tokens del contrato 
+    //     uint256 tbalance =  this.balanceOf(contrato);
         
-        // Filtro para evaluar los tokens a comprar con los tokens disponibles 
-        require (_value <= tbalance, "Compra un numero de Tokens adecuado.");
+    //     // Filtro para evaluar los tokens a comprar con los tokens disponibles 
+    //     require (_value <= tbalance, "Compra un numero de Tokens adecuado.");
        
-        // Tranferencia de Tokens al comprador  
-        this.transfer(_to, _value);
+    //     // Tranferencia de Tokens al comprador  
+    //     this.transfer(_to, _value);
        
-       // Emitir el evento de compra tokens 
-        emit PaymentSent(_to, _value);
-    }
+    //    // Emitir el evento de compra tokens 
+    //     emit PaymentSent(_to, _value);
+    // }
 
     // Función para obtener el balance del contrato
-    function getContractBalance() external view returns (uint256) {
-        return contrato.balance;
+    function contractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+     function addressBalance(address _to) external view returns (uint256) {
+        return _to.balance;
     }
 
 
--- contracts/utils/SafeMath.sol
+++ contracts/utils/SafeMath.sol
@@ -22,13 +22,37 @@ library SafeMath{
     
     // Multiplicacion
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
+      if (a == 0) {
+        return 0;
+      }
 
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
+      uint256 c = a * b;
+      require(c / a == b, "SafeMath: multiplication overflow");
 
-        return c;
+      return c;
     }
+
+    // Función para convertir un valor en wei a ETH
+    function convertToEth(uint256 valueInWei) internal pure returns (uint256) {
+      // 1 ETH = 10^18 wei
+      uint256 ethValue = valueInWei / 1 ether;
+      return ethValue;
+    }
+
+    // Función para convertir un valor en ETH a wei
+    function convertToWei(uint256 valueInEth) internal pure returns (uint256) {
+      // 1 ETH = 10^18 wei
+      uint256 weiValue = valueInEth * 1 ether;
+      return weiValue;
+    }
+ 
+    function discountTransaction(uint256 number) internal pure returns (uint256){
+      // Calcula el 2% del número
+      return  mul(number, 2) / 100;
+    }
+
+    function costValue(uint256 number) internal pure returns (uint256){
+      return number*(1 ether);
+    }
+
 }
