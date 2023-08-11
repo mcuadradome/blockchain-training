627c080a11203e0ef9fac9bffb139fc54d43e715
mcuadrado <mcuadradome@gmail.com>
2023-08-11 17:13:52 -0500

    remove only owner

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-import "./token/ERC20.sol";
+import "./utils/SafeMath.sol";
 import "./utils/Context.sol";
 
 contract Main {
@@ -13,11 +13,8 @@ contract Main {
     */
     using SafeMath for uint256;
 
-    address public owner;
+    address private owner;
     //mapping(address => uint256) public balances;
-
-    // Instancia del contrato Token 
-    // ERC20 private token;
     
     // Direcciones ;
     address payable  public contrato;
@@ -31,8 +28,6 @@ contract Main {
  
 
     constructor(){ 
-        // totalSupply_ = total;
-        // token = new ERC20("MyToken", "MTK");
         owner = msg.sender;
         contrato = payable(address(this));    
     }
@@ -46,18 +41,8 @@ contract Main {
         return (_address != address(0) && _address != address(contrato) && bytes20(_address).length == 20);
     }
 
-    // function addEthToBalance(uint256 amount) payable external onlyOwner()  {
-    //     require(msg.value > 0, "Debe enviar ETH");
-    //     require(msg.value ==  SafeMath.convertToWei(amount), "El valor ingresado no es valido");
-    //     require(this.contractBalance() > 0, "El contrato no tiene saldo para procesar la transaccion");
-
-    //     uint256 balance = SafeMath.add(this.contractBalance(), msg.value); 
-    //     payable(msg.sender).transfer(balance);
-    // }
-
     // Función para agregar ETH a una dirección específica
-    function addEthToAddress(address payable _receiver) external payable onlyOwner() {
-        //require(_receiver != address(0), "La direccion del receptor no puede ser 0x0");
+    function addEthToAddress(address payable _receiver) external payable {
         require(isValidAddress(_receiver), "La direccion del receptor no es valida");
         require(msg.value > 0 && msg.value <= maxEthPerTransaction, "Cantidad de ETH no valida");
         require(this.addressBalance(msg.sender) > 0, "La cuenta origen no tiene saldo"); 
@@ -68,7 +53,7 @@ contract Main {
     }
 
         // Función para dividir un monto entre dos direcciones
-    function payment(address payable _receiver) payable external onlyOwner(){
+    function payment(address payable _receiver) payable external {
         require(msg.value > 0 && msg.value <= maxEthPerTransaction, "Cantidad de ETH no valida");
         require(this.addressBalance(msg.sender) == 0, "La cuenta origen no tiene saldo"); 
         require(isValidAddress(_receiver), "La direccion del receptor no es valida");
@@ -84,42 +69,6 @@ contract Main {
         _receiver.transfer(remainingAmount);
         emit PaymentReceived(_receiver, remainingAmount);
     }
-
-    // function balance() public view returns (uint256) {
-    //     return token.balanceOf(contrato);
-    // }
-
-    // function GeneraTokens(address account, uint amount) public onlyOwner(){
-    //     _mint(account, amount);
-    // }
-
-    // function paymentValue(address _to, uint256 _value) external payable {
- 
-    //     uint coste = costValue(_value);
-    //     // Se requiere que el valor de ethers pagados sea equivalente al coste 
-    //     require (_value >= coste, "Compra menos Tokens o paga con mas Ethers.");
-        
-    //     // Diferencia a pagar 
-    //     uint256 returnValue = _value - coste;
-        
-    //     // Tranferencia de la diferencia 
-    //     payable(_to).transfer(returnValue);
-
-    //     // Tranferencia  el coste a la cuenta intermediaria 
-    //     payable(intermediary).transfer(coste);
-       
-    //     // Obtener el balance de Tokens del contrato 
-    //     uint256 tbalance =  this.balanceOf(contrato);
-        
-    //     // Filtro para evaluar los tokens a comprar con los tokens disponibles 
-    //     require (_value <= tbalance, "Compra un numero de Tokens adecuado.");
-       
-    //     // Tranferencia de Tokens al comprador  
-    //     this.transfer(_to, _value);
-       
-    //    // Emitir el evento de compra tokens 
-    //     emit PaymentSent(_to, _value);
-    // }
 
     // Función para obtener el balance del contrato
     function contractBalance() external view returns (uint256) {
